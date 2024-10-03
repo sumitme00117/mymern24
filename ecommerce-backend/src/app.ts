0dd6297e6990129e6fc3af7dbb0077cbed91d617
Sumit Srivastava <sumitme00117@gmail.com>
2024-10-03 11:49:08 +0530

    App Changes

---
+++ ecommerce-backend/src/app.ts
@@ -0,0 +1,70 @@
+import express, { NextFunction, Request, Response } from 'express'
+import { connectDB, connectRedis } from './utils/features.js'
+import { errorMiddleware } from './middlewares/error.js'
+import NodeCache from 'node-cache'
+import {config} from 'dotenv'
+import morgan from 'morgan'
+import Stripe from 'stripe'
+
+// Importing Routes
+import userRoute from "./routes/user.js"
+import productRoute from "./routes/products.js"
+import orderRoute from "./routes/order.js"
+import paymentRoute from "./routes/payment.js"
+import dashboardRoute from "./routes/stats.js"
+import cors from "cors"
+import { v2 as cloudinary } from "cloudinary";
+
+
+
+
+config({path: "./.env"})
+const port = process.env.PORT || 4000
+const mongoURI = process.env.MONGO_URI || ""
+const stripeKey = process.env.STRIPE_KEY || ""
+
+const redisURI = process.env.REDIS_URI || "";
+export const redisTTL = process.env.REDIS_TTL || 60 * 60 * 4;
+connectDB(mongoURI)
+
+export const redis = connectRedis(redisURI);
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
+  });
+  
+export const stripe = new Stripe(stripeKey)
+
+export const myCache = new NodeCache()
+const app = express()
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(cors({
+    origin: [process.env.CLIENT_URL!],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }))
+app.get("/", (req,res) => {
+    res.send("API working with /api/v1")
+})
+
+// Using Routes
+app.use("/api/v1/user", userRoute)
+app.use("/api/v1/product", productRoute)
+app.use("/api/v1/order", orderRoute)
+app.use("/api/v1/payment", paymentRoute)
+app.use("/api/v1/dashboard", dashboardRoute)
+
+
+
+
+
+app.use("/uploads", express.static("uploads"))
+app.use(errorMiddleware)
+
+
+app.listen(port, () => {
+    console.log(`Server is working on http://localhost:${port}`)
+})
