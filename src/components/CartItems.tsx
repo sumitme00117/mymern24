b6fe325d63d97dca56c26557bcb747d38cb9feae
Sumit Srivastava <sumitme00117@gmail.com>
2024-10-10 11:36:18 +0530

    Cart Item Changes

--- src/components/CartItems.tsx
+++ src/components/CartItems.tsx
@@ -1,32 +1,44 @@
-import { FaTrash } from "react-icons/fa"
-import { Link } from "react-router-dom"
+
+
+import { FaTrash } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { CartItem } from "../types/types";
+import { transformImage } from "../utils/features";
 
 type CartItemProps = {
-    cartItem: any
-}
+  cartItem: CartItem;
+  incrementHandler: (cartItem: CartItem) => void;
+  decrementHandler: (cartItem: CartItem) => void;
+  removeHandler: (id: string) => void;
+};
 
+const CartItemComponent = ({
+  cartItem,
+  incrementHandler,
+  decrementHandler,
+  removeHandler,
+}: CartItemProps) => {
+  const { photo, productId, name, price, quantity } = cartItem;
 
-
-const CartItem = ({cartItem}: CartItemProps) => {
-
-    const {photo,productId,name,price,quantity} = cartItem
   return (
     <div className="cart-item">
-      <img src={photo} alt={name}/>
+      <img src={transformImage(photo)} alt={name} />
       <article>
         <Link to={`/product/${productId}`}>{name}</Link>
-        <span>₹ {price}</span>
+        <span>₹{price}</span>
       </article>
 
       <div>
-        <button>-</button>
+        <button onClick={() => decrementHandler(cartItem)}>-</button>
         <p>{quantity}</p>
-        <button>+</button>
+        <button onClick={() => incrementHandler(cartItem)}>+</button>
       </div>
-        <button><FaTrash/></button>
 
+      <button onClick={() => removeHandler(productId)}>
+        <FaTrash />
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItemComponent;
