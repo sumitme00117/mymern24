4db826126ecf57a41bd953dc5b204ea6abe2a7a7
Sumit Srivastava <sumitme00117@gmail.com>
2024-10-12 10:57:28 +0530

    Product Card Component

--- src/components/Product-Card.tsx
+++ src/components/Product-Card.tsx
@@ -1,29 +1,58 @@
-import { FaPlus } from "react-icons/fa";
+
+
+import { FaExpandAlt, FaPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { CartItem } from "../types/types";
+import { transformImage } from "../utils/features";
 
 type ProductsProps = {
   productId: string;
-  photo: string;
+  photos: {
+    url: string;
+    public_id: string;
+  }[];
   name: string;
   price: number;
   stock: number;
-  handler: () => void;
-}
+  handler: (cartItem: CartItem) => string | undefined;
+};
 
-const server = "dggef"
-
-const ProductCard = ({productId,price,name,photo,stock,handler}: ProductsProps) => {
+const ProductCard = ({
+  productId,
+  price,
+  name,
+  photos,
+  stock,
+  handler,
+}: ProductsProps) => {
   return (
     <div className="product-card">
-      <img src={photo} alt={name} />
+      <img src={transformImage(photos?.[0]?.url, 400)} alt={name} />
       <p>{name}</p>
       <span>₹{price}</span>
+
       <div>
-        <button onClick={() => handler()}>
-          <FaPlus/>
+        <button
+          onClick={() =>
+            handler({
+              productId,
+              price,
+              name,
+              photo: photos[0].url,
+              stock,
+              quantity: 1,
+            })
+          }
+        >
+          <FaPlus />
         </button>
+
+        <Link to={`/product/${productId}`}>
+          <FaExpandAlt />
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
