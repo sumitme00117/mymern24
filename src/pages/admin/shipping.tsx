1b5bf9ef236901474d2a3275ee27c55b738baa70
Sumit Srivastava <sumitme00117@gmail.com>
2024-10-14 00:28:31 +0530

    Shipping page changes

--- src/pages/admin/shipping.tsx
+++ src/pages/admin/shipping.tsx
@@ -1,17 +1,20 @@
+import axios from "axios";
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { BiArrowBack } from "react-icons/bi";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { CartReducerInitialState } from "../types/reducer-types";
-import { server } from "../redux/store";
-import axios from "axios";
-import toast from "react-hot-toast";
 import { saveShippingInfo } from "../redux/reducer/cartReducer";
+import { RootState, server } from "../redux/store";
 
 const Shipping = () => {
-  const { cartItems, total } = useSelector(
-    (state: { cartReducer: CartReducerInitialState }) => state.cartReducer
+  const { cartItems, coupon } = useSelector(
+    (state: RootState) => state.cartReducer
   );
+  const { user } = useSelector((state: RootState) => state.userReducer);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const [shippingInfo, setShippingInfo] = useState({
     address: "",
@@ -20,10 +23,6 @@ const Shipping = () => {
     country: "",
     pinCode: "",
   });
-
-  const navigate = useNavigate();
-
-  const dispatch = useDispatch();
 
   const changeHandler = (
     e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
@@ -35,15 +34,27 @@ const Shipping = () => {
     e.preventDefault();
 
     dispatch(saveShippingInfo(shippingInfo));
+
     try {
       const { data } = await axios.post(
-        `${server}/api/v1/payment/create`,
-        { amount: total },
-        { headers: { "Content-Type": "application/json" } }
+        `${server}/api/v1/payment/create?id=${user?._id}`,
+        {
+          items: cartItems,
+          shippingInfo,
+          coupon,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
       );
 
-      navigate("/pay", { state: data.clientSecret });
+      navigate("/pay", {
+        state: data.clientSecret,
+      });
     } catch (error) {
+      console.log(error);
       toast.error("Something went wrong");
     }
   };
@@ -51,6 +62,7 @@ const Shipping = () => {
   useEffect(() => {
     if (cartItems.length <= 0) return navigate("/cart");
   }, [cartItems]);
+
   return (
     <div className="shipping">
       <button className="back-btn" onClick={() => navigate("/cart")}>
@@ -59,6 +71,7 @@ const Shipping = () => {
 
       <form onSubmit={submitHandler}>
         <h1>Shipping Address</h1>
+
         <input
           required
           type="text"
@@ -67,6 +80,7 @@ const Shipping = () => {
           value={shippingInfo.address}
           onChange={changeHandler}
         />
+
         <input
           required
           type="text"
@@ -75,6 +89,7 @@ const Shipping = () => {
           value={shippingInfo.city}
           onChange={changeHandler}
         />
+
         <input
           required
           type="text"
@@ -83,6 +98,7 @@ const Shipping = () => {
           value={shippingInfo.state}
           onChange={changeHandler}
         />
+
         <select
           name="country"
           required
@@ -92,6 +108,7 @@ const Shipping = () => {
           <option value="">Choose Country</option>
           <option value="india">India</option>
         </select>
+
         <input
           required
           type="number"
@@ -100,6 +117,7 @@ const Shipping = () => {
           value={shippingInfo.pinCode}
           onChange={changeHandler}
         />
+
         <button type="submit">Pay Now</button>
       </form>
     </div>
