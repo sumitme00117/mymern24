fa2cc8d4b7733b3e0dfd7fab34c10aa5d846e622
Sumit Srivastava <sumitme00117@gmail.com>
2024-11-03 16:47:46 +0530

    User API

--- src/redux/api/userAPI.ts
+++ src/redux/api/userAPI.ts
@@ -1,8 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-import { AllUsersResponse, DeleteUserRequest, MessageResponse, UserResponse } from "../../types/api-types";
-import { User } from "../../types/types";
 import axios from "axios";
+import {
+  AllUsersResponse,
+  DeleteUserRequest,
+  MessageResponse,
+  UserResponse,
+} from "../../types/api-types";
+import { User } from "../../types/types";
 
 export const userAPI = createApi({
   reducerPath: "userApi",
@@ -17,28 +21,27 @@ export const userAPI = createApi({
         method: "POST",
         body: user,
       }),
-      invalidatesTags: ["users"]
+      invalidatesTags: ["users"],
     }),
 
     deleteUser: builder.mutation<MessageResponse, DeleteUserRequest>({
-      query: ({userId, adminUserId}) => ({
+      query: ({ userId, adminUserId }) => ({
         url: `${userId}?id=${adminUserId}`,
         method: "DELETE",
       }),
-      invalidatesTags: ["users"]
+      invalidatesTags: ["users"],
     }),
-  
-    allUsers: builder.query<AllUsersResponse,string>({
-      query: (id) => `all?id=${id}`,
-      providesTags: ["users"]
-    })
 
+    allUsers: builder.query<AllUsersResponse, string>({
+      query: (id) => `all?id=${id}`,
+      providesTags: ["users"],
+    }),
   }),
 });
 
 export const getUser = async (id: string) => {
   try {
-    const { data }: {data: UserResponse} = await axios.get(
+    const { data }: { data: UserResponse } = await axios.get(
       `${import.meta.env.VITE_SERVER}/api/v1/user/${id}`
     );
 
@@ -48,4 +51,5 @@ export const getUser = async (id: string) => {
   }
 };
 
-export const { useLoginMutation, useAllUsersQuery, useDeleteUserMutation } = userAPI;
+export const { useLoginMutation, useAllUsersQuery, useDeleteUserMutation } =
+  userAPI;
