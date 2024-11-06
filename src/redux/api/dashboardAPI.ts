744bece2ad064b2899da661f23a90f7d52d3d017
Sumit Srivastava <sumitme00117@gmail.com>
2024-11-06 12:48:43 +0530

    Dashboard API

--- src/redux/api/dashboardAPI.ts
+++ src/redux/api/dashboardAPI.ts
@@ -1,5 +1,11 @@
+
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { BarResponse, LineResponse, PieResponse, StatsResponse } from "../../types/api-types";
+import {
+  BarResponse,
+  LineResponse,
+  PieResponse,
+  StatsResponse,
+} from "../../types/api-types";
 
 export const dashboardApi = createApi({
   reducerPath: "dashboardApi",
@@ -9,21 +15,22 @@ export const dashboardApi = createApi({
   endpoints: (builder) => ({
     stats: builder.query<StatsResponse, string>({
       query: (id) => `stats?id=${id}`,
-      keepUnusedDataFor: 0
+      keepUnusedDataFor: 0,
     }),
     pie: builder.query<PieResponse, string>({
       query: (id) => `pie?id=${id}`,
-      keepUnusedDataFor: 0
+      keepUnusedDataFor: 0,
     }),
     bar: builder.query<BarResponse, string>({
       query: (id) => `bar?id=${id}`,
-      keepUnusedDataFor: 0
+      keepUnusedDataFor: 0,
     }),
     line: builder.query<LineResponse, string>({
       query: (id) => `line?id=${id}`,
-      keepUnusedDataFor: 0
+      keepUnusedDataFor: 0,
     }),
   }),
 });
 
-export const {useBarQuery,usePieQuery,useLineQuery,useStatsQuery} = dashboardApi;
+export const { useBarQuery, useStatsQuery, useLineQuery, usePieQuery } =
+  dashboardApi;
