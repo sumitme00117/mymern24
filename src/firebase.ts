3242dc3b7734009614fffacde7770cc20d10a3bd
Sumit Srivastava <sumitme00117@gmail.com>
2024-11-28 19:32:48 +0530

    Firebase file updated

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
 
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
@@ -8,10 +9,8 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+  appId: import.meta.env.VITE_APP_ID,
 };
 
-
 export const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app)
+export const auth = getAuth(app);
