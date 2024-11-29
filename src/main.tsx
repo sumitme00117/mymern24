10288cb461e681bd12bbd741e27325996dfb0386
Sumit Srivastava <sumitme00117@gmail.com>
2024-11-29 12:33:20 +0530

    Main file Updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/app.scss";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>
+  </React.StrictMode>
 );
-
