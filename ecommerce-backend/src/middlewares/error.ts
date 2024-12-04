f176290a1011b58073597447e25ff83df5309101
Sumit Srivastava <sumitme00117@gmail.com>
2024-12-05 00:50:25 +0530

    Updated error file

--- ecommerce-backend/src/middlewares/error.ts
+++ ecommerce-backend/src/middlewares/error.ts
@@ -1,20 +1,28 @@
-import { NextFunction, Request, Response } from 'express'
-import ErrorHandler from '../utils/utility-class.js';
-import { ControllerType } from '../types/types.js';
 
 
-export const errorMiddleware = (err: ErrorHandler,req: Request,res: Response,next: NextFunction) => {
+import { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../utils/utility-class.js";
+import { ControllerType } from "../types/types.js";
 
-    err.message ||= "Internal Server Error";
-    err.statusCode ||= 500
+export const errorMiddleware = (
+  err: ErrorHandler,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  err.message ||= "Internal Server Error";
+  err.statusCode ||= 500;
 
-    if(err.name === "CastError") err.message = "Invalid Id"
-    return res.status(err.statusCode).json({
-        success: false,
-        message: err.message
-    })
-}
+  if (err.name === "CastError") err.message = "Invalid ID";
 
-export const TryCatch = (func: ControllerType) => (req: Request, res: Response, next: NextFunction) => {
-    return Promise.resolve(func(req,res,next)).catch(next)
-}
+  return res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
+
+export const TryCatch =
+  (func: ControllerType) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    return Promise.resolve(func(req, res, next)).catch(next);
+  };
