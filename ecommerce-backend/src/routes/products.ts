63238ff77b90eefee66a8112a12d65807b782b32
Sumit Srivastava <sumitme00117@gmail.com>
2024-12-10 23:52:04 +0530

    Product route changes

--- ecommerce-backend/src/routes/products.ts
+++ ecommerce-backend/src/routes/products.ts
@@ -1,16 +1,48 @@
+
+
 import express from "express";
-import { deleteProduct, getAdminProducts, getAllCategories, getAllProducts, getlatestProducts, getSingleProduct, newProduct, updateProduct } from "../controllers/product.js";
-import { singleUpload, mutliUpload } from "../middlewares/multer.js";
+import {
+  allReviewsOfProduct,
+  deleteProduct,
+  deleteReview,
+  getAdminProducts,
+  getAllCategories,
+  getAllProducts,
+  getSingleProduct,
+  getlatestProducts,
+  newProduct,
+  newReview,
+  updateProduct,
+} from "../controllers/product.js";
 import { adminOnly } from "../middlewares/auth.js";
+import { mutliUpload } from "../middlewares/multer.js";
 
+const app = express.Router();
 
-const app = express.Router()
+//To Create New Product  - /api/v1/product/new
+app.post("/new", adminOnly, mutliUpload, newProduct);
 
-app.post("/new", adminOnly, mutliUpload, newProduct)
-app.get("/latest", getlatestProducts)
-app.get("/all", getAllProducts)
-app.get("/categories",getAllCategories)
-app.get("/admin-products", adminOnly, getAdminProducts)
-app.route("/:id").get(getSingleProduct).put(adminOnly, mutliUpload, updateProduct).delete(adminOnly, deleteProduct)
+//To get all Products with filters  - /api/v1/product/all
+app.get("/all", getAllProducts);
 
-export default app
+//To get last 10 Products  - /api/v1/product/latest
+app.get("/latest", getlatestProducts);
+
+//To get all unique Categories  - /api/v1/product/categories
+app.get("/categories", getAllCategories);
+
+//To get all Products   - /api/v1/product/admin-products
+app.get("/admin-products", adminOnly, getAdminProducts);
+
+// To get, update, delete Product
+app
+  .route("/:id")
+  .get(getSingleProduct)
+  .put(adminOnly, mutliUpload, updateProduct)
+  .delete(adminOnly, deleteProduct);
+
+app.get("/reviews/:id", allReviewsOfProduct);
+app.post("/review/new/:id", newReview);
+app.delete("/review/:id", deleteReview);
+
+export default app;
