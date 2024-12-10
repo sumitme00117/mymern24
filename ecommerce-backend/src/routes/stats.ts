b02e203c774bfc700e59c718c190543ecd865a30
Sumit Srivastava <sumitme00117@gmail.com>
2024-12-10 23:51:26 +0530

    Stats route changes

--- ecommerce-backend/src/routes/stats.ts
+++ ecommerce-backend/src/routes/stats.ts
@@ -1,15 +1,26 @@
+
+
 import express from "express";
 import { adminOnly } from "../middlewares/auth.js";
-import { getBarCharts, getDashboardStats, getLineCharts, getPieCharts } from "../controllers/stats.js";
+import {
+  getBarCharts,
+  getDashboardStats,
+  getLineCharts,
+  getPieCharts,
+} from "../controllers/stats.js";
 
-const app = express.Router()
+const app = express.Router();
 
-// route - /api/v1/dashboard
-app.get("/stats", adminOnly, getDashboardStats)
-app.get("/pie", adminOnly, getPieCharts)
-app.get("/bar", adminOnly, getBarCharts)
-app.get("/line", adminOnly, getLineCharts)
+// route - /api/v1/dashboard/stats
+app.get("/stats", adminOnly, getDashboardStats);
 
+// route - /api/v1/dashboard/pie
+app.get("/pie", adminOnly, getPieCharts);
 
+// route - /api/v1/dashboard/bar
+app.get("/bar", adminOnly, getBarCharts);
 
-export default app
+// route - /api/v1/dashboard/line
+app.get("/line", adminOnly, getLineCharts);
+
+export default app;
